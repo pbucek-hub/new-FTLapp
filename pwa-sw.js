6b35a7fb940092af591b77cb47d4202efb7d8613
pbucek-hub <pbucek@mac.com>
2026-09-25 11:39:33 +0200

    Publish non-caching FTL PWA service worker

---
+++ pwa-sw.js
@@ -0,0 +1,5 @@
+self.addEventListener('install', event => { self.skipWaiting(); });
+self.addEventListener('activate', event => { event.waitUntil(self.clients.claim()); });
+self.addEventListener('fetch', event => {
+  // Network passthrough by design: do not cache operational FTL logic.
+});
